451c72747750d19494a926657850e6dfe3c356ee
wanjiang <lily@lilymatoMacBook-Pro.local>
2015-04-05 17:35:23 -0500

    db

--- dreamycity/settings.py
+++ dreamycity/settings.py
@@ -57,8 +57,12 @@ WSGI_APPLICATION = 'dreamycity.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'dreamycity',
+        'USER':'dreamycity',
+        'PASSWORD':'forever',
+        'HOST':'localhost',
+        'POST':'3306',
     }
 }
 
